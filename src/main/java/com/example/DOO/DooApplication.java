31cd71ec37d40c6b8f4aa7e950ccc46f8c752ed4
A-shoking <ashokkuamrgo190@gmail.com>
2025-01-27 18:30:46 -0600

    Project Intilized

---
+++ src/main/java/com/example/DOO/DooApplication.java
@@ -0,0 +1,13 @@
+package com.example.DOO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DooApplication.class, args);
+	}
+
+}
