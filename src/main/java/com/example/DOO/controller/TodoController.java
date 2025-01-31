216e0599476a497d24556f5ed6274252346b4133
A-shoking <ashokkuamrgo190@gmail.com>
2025-01-30 21:23:04 -0600

    Update api

--- src/main/java/com/example/DOO/controller/TodoController.java
+++ src/main/java/com/example/DOO/controller/TodoController.java
@@ -1,10 +1,18 @@
 package com.example.DOO.controller;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +28,37 @@ public class TodoController {
 
 	@GetMapping("/todos")
 	public List<Todo> getAllTodos() {
+		int x = 5; 
 		return todoService.getAllTodos();
 	}
+	
+	
+//	@GetMapping("/todosUse/{id}")
+//	public Optional<Todo> getIDTodos(@PathParam(value = "") Long id){
+//	
+//		return todoService.getById(id);
+//	}
+	
+	@GetMapping("todosUse/{id}")
+	public Optional<Todo> getIDTodos(@PathVariable Long id) {
+	    return todoService.getById(id);
+	}
+	
+//	  @GetMapping("todosUs/{id}")
+//	    public ResponseEntity<Todo> getTodoById(@PathVariable Long id) {
+//	        Optional<Todo> todo = todoService.getTodoById(id);
+//	        return todo.map(ResponseEntity::ok)
+//	                   .orElseGet(() -> ResponseEntity.notFound().build());
+//	    }
+
+	
+	  @PostMapping("create")
+	    public ResponseEntity<Todo> createTodo(@RequestBody Map<String, String> requestBody) {
+	        Todo createdTodo = todoService.createTodo(
+	                requestBody.get("heading"),
+	                requestBody.get("progress"),
+	                requestBody.get("date")
+	        );
+	        return ResponseEntity.status(201).body(createdTodo);
+	    }
 }
--- src/main/java/com/example/DOO/service/TodoService.java
+++ src/main/java/com/example/DOO/service/TodoService.java
@@ -2,21 +2,43 @@ package com.example.DOO.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
+import javax.swing.text.html.Option;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.DOO.model.Todo;
-
+import com.example.DOO.repo.CreateRepository;
 import com.example.DOO.repo.TodoRepository;
-
+import java.util.Optional;
 @Service
 public class TodoService {
 
 	@Autowired
 	private TodoRepository todoRepository;
+	
+	@Autowired
+	private CreateRepository createTodo;
 
 	public List<Todo> getAllTodos() {
 		return todoRepository.findAll();
 	}
+
+	public Optional<Todo> getById(Long id) {
+		return todoRepository.findById(id);
+		 // return Optional.ofNullable(todoRepository.getById(id));
+	}
+	public Optional<Todo> getTodoById(Long id) {
+        return todoRepository.findById(id);
+    }
+	
+	 public Todo createTodo(String heading, String progress, String date) {
+	        Todo todo = new Todo();
+	        todo.setTitle(heading);
+	        todo.setStatus(progress);
+	        todo.setDate(date);
+	        return todoRepository.save(todo);
+	    }
 }
